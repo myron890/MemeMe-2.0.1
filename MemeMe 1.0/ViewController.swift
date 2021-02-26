c81180385678ba68d6f48627ed537e7967d5cb90
Myron <myron890@gmail.com>
2021-02-26 08:28:39 +0200

    Add an Outlet for memePlaceholder and an Action for toolbar button

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var memePlaceholder: UIImageView!
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
